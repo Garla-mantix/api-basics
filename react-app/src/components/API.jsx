ab099c2464a7f089a5702285c24a7353da97bc26
Luca Pirro <aspudds-luca@hotmail.com>
2025-05-06 15:50:30 +0200

    cleanup and enter

--- react-app/src/components/API.jsx
+++ react-app/src/components/API.jsx
@@ -15,14 +15,30 @@ const API = () => {
         }
     };
 
-    useEffect(() => {
+    const showAdvice = () => {
         fetchData();
-    }, []);
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            showAdvice();
+        }
+    };
 
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-             <input placeholder="What do you want to ask The Oracle?" class="bg-gray-200  mb-8 max-w-100 text-center appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text"></input>
-            <button className="bg-blue-500 hover:bg-blue-700 mb-8 text-white font-bold py-2 px-4 border border-blue-700 rounded">Ask</button>
+             <input 
+                placeholder="What do you want to ask The Oracle?" 
+                className="bg-gray-200  mb-8 max-w-100 text-center appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
+                id="inline-full-name" 
+                type="text"
+                onKeyDown={handleKeyDown}
+             />
+            <button 
+                onClick={showAdvice} 
+                className="bg-blue-500 hover:bg-blue-700 mb-8 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+            >
+                Ask </button>
             <p className="max-w-100"><span className="font-bold">The oracle's advice: </span>{advice.fact}</p>
         </div>
 
