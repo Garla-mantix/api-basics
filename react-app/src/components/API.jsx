8d05eda6287db2057f6d9e63e55ab8f169dfea33
Luca Pirro <aspudds-luca@hotmail.com>
2025-05-06 16:39:26 +0200

    margin

--- react-app/src/components/API.jsx
+++ react-app/src/components/API.jsx
@@ -35,7 +35,7 @@ const API = () => {
 
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-600 text-gray-50 ">
-            <h1 className="mb-20 font-bold font-serif text-2xl">Ask The Oracle</h1>
+            <h1 className="mb-15 font-bold font-serif text-2xl">Ask The Oracle</h1>
              <input 
                 placeholder="What do you want to ask The Oracle?" 
                 className="bg-gray-200  mb-8 max-w-100 text-center appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
