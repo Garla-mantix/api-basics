9e23825107f2e94567203eda4d422349902ec724
Luca Pirro <aspudds-luca@hotmail.com>
2025-05-06 16:37:15 +0200

    clearing input and display question

--- react-app/src/components/API.jsx
+++ react-app/src/components/API.jsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 const API = () => {
 
     const [advice, setAdvice] = useState({});
+    const [inputValue, setInputValue] = useState("");
+    const [userQuestion, setUserQuestion] = useState("");
 
     const fetchData = async () => {
         try {
@@ -16,13 +18,19 @@ const API = () => {
     };
 
     const showAdvice = () => {
+        setUserQuestion(inputValue);
         fetchData();
+        setInputValue("");
     };
 
     const handleKeyDown = (event) => {
         if (event.key === "Enter") {
             showAdvice();
         }
+    };
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
     };
 
     return (
@@ -33,14 +41,26 @@ const API = () => {
                 className="bg-gray-200  mb-8 max-w-100 text-center appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
                 id="inline-full-name" 
                 type="text"
+                value={inputValue}
                 onKeyDown={handleKeyDown}
+                onChange={handleInputChange}
              />
             <button 
                 onClick={showAdvice} 
                 className="bg-blue-500 hover:bg-blue-700 mb-8 text-white font-bold py-2 px-4 border border-blue-700 rounded"
             >
-                Ask </button>
-            <p className="max-w-100"><span className="font-bold text-amber-300">The oracle's advice: </span>{advice.fact}</p>
+                Ask 
+            </button>
+            <div>
+            {userQuestion && (
+                <p className="max-w-100">
+                    <span className="font-bold text-amber-300">Your question: </span>{userQuestion}
+                </p>
+            )}
+            <p className="max-w-100"><span className="font-bold text-green-400">The oracle's advice: </span>{advice.fact}</p>
+
+            </div>
+           
         </div>
 
 
