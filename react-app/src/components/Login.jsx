eba35cb3a7ac7055332063305aa08c977a70ce43
Luca Pirro <aspudds-luca@hotmail.com>
2025-05-07 14:57:20 +0200

    small fix

--- react-app/src/components/Login.jsx
+++ react-app/src/components/Login.jsx
@@ -50,6 +50,7 @@ const Login = ({onLogin}) => {
                             type="text"
                             value={username}
                             onChange={(e) => setUsername(e.target.value)}
+                            onKeyDown={handleKeyDown}
                             required
                             autoFocus
                             className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
